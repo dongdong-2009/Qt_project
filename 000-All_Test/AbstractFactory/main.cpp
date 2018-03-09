64d4dd6e821aed844d93c3bc258dd23fae1753eb
lb9726 <1343726739@qq.com>
2018-03-09 20:41:56 +0800

    add factory and bulider design

--- 000-All_Test/AbstractFactory/main.cpp
+++ 000-All_Test/AbstractFactory/main.cpp
@@ -5,11 +5,27 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     AbstractFactory* cf1 = new ConcreteFactory1();
-    cf1->CreateProductA();
-    cf1->CreateProductB();
+    AbstractProductA* aba =  cf1->CreateProductA();
+    AbstractProductB* abb =  cf1->CreateProductB();
 
     AbstractFactory* cf2 = new ConcreteFactory2();
-    cf2->CreateProductA();
-    cf2->CreateProductB();
+    AbstractProductA* aba2 =  cf2->CreateProductA();
+    AbstractProductB* abb2 = cf2->CreateProductB();
+
+    delete cf1;
+    cf1 = 0;
+    delete cf2;
+    cf2 = 0;
+
+    delete aba;
+    aba = 0;
+    delete abb;
+    abb = 0;
+
+    delete aba2;
+    aba2 = 0;
+    delete abb2;
+    abb2 = 0;
+
     return 0;
 }
